a6cb5e95c274cb4ef58e9ff23fa149b3de51326d
Jackie <taekwondo.chan@gmail.com>
2021-02-26 21:06:37 -0800

    fix broken category names

--- exercise_app/models.py
+++ exercise_app/models.py
@@ -11,11 +11,17 @@ class Exercise(db.Model):
     category = db.relationship('Category', back_populates='exercises')
     favorited_by = db.relationship('User', secondary='favorite_exercises_table', back_populates='favorite_exercises')
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Category(db.Model):
     '''Category model'''
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), nullable = False)
     exercises = db.relationship('Exercise', back_populates='category')
+
+    def __str__(self):
+        return f'{self.name}'
 
 class User(UserMixin, db.Model):
     '''User model'''
@@ -24,6 +30,9 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(200), nullable=False)
     favorite_exercises = db.relationship('Exercise', secondary='favorite_exercises_table', back_populates='favorited_by')
 
+    def __str__(self):
+        return f'{self.username}'
+        
 favorite_user_exercises = db.Table('favorite_exercises_table', 
     db.Column('exercise_id', db.Integer, db.ForeignKey('exercise.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
