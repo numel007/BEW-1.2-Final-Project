c7b13fdbcb0be98381574cf47064bd057955947b
Jackie <taekwondo.chan@gmail.com>
2021-03-02 15:12:24 -0800

    configure database url

--- exercise_app/config.py
+++ exercise_app/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    # Matches Heroku database url
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
